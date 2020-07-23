e35d6c109e547a414e64fc374cd28ae96175ffc3
Alvaro Pereira do Nascimento <alvaro.nascimentoapn@gmail.com>
2020-07-23 14:29:26 -0300

    Adicionar arquivos de projeto.

---
+++ RevisãoDigitalInovationOne/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RevisãoDigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObterOpcaoUsuario();
+
+            String OpcaoUsuario = Console.ReadLine();
+
+            while (OpcaoUsuario.ToUpper() != "X")
+            {
+                switch (OpcaoUsuario)
+                {
+                    case "1":
+                        //TODo: adicionar aluno.
+                        break;
+                    case "2":
+                        //TODO: listar alunos.
+                        break;
+                    case "3":
+                        //TODO:calucular media geral.
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("Opção inválida");
+                }
+
+
+                ObterOpcaoUsuario();
+
+                OpcaoUsuario = Console.ReadLine();
+            }
+
+            Console.WriteLine("Sistema finalizado!!");
+
+        }
+
+        private static void ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada ");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - sair");
+            Console.WriteLine();
+        }
+    }
+}
